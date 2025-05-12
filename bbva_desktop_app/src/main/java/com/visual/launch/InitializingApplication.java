29789ff124dd46dba5ed1061978b40d4e0c0f274
ithan <ithan@LAPTOP-SQRUGF89>
2025-05-11 20:28:41 -0600

    aplicar los nuevos cambios a la ventana de inicialización

--- bbva_desktop_app/src/main/java/com/visual/launch/InitializingApplication.java
+++ bbva_desktop_app/src/main/java/com/visual/launch/InitializingApplication.java
@@ -17,6 +17,7 @@ import javax.swing.UIManager;
 
 import main.java.com.visual.access.ClientRedirection;
 import main.resources.visual.manager.classes.IconImageManager;
+import javax.swing.ImageIcon;
 
 public class InitializingApplication extends JDialog {
 
@@ -27,7 +28,8 @@ public class InitializingApplication extends JDialog {
 	
 	private IconImageManager icon = new IconImageManager();
 	private JLabel lbl_banner_logo_company;
-	private JLabel lbl_loading_application_message;
+	private JLabel lbl_mssg_loading_software;
+	private JLabel lbl_animated_loading;
 	
 	/**
 	 * Launch the application.
@@ -76,14 +78,25 @@ public class InitializingApplication extends JDialog {
 		lbl_banner_logo_company.setHorizontalAlignment(SwingConstants.RIGHT);
 		containerWindow.add(lbl_banner_logo_company);
 		
-		lbl_loading_application_message = new JLabel();
-		lbl_loading_application_message.setVerticalAlignment(SwingConstants.TOP);
-		lbl_loading_application_message.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_loading_application_message.setText("Cargando aplicación . . .");
-		lbl_loading_application_message.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 20));
-		lbl_loading_application_message.setForeground(new Color(255,255,255));
-		lbl_loading_application_message.setBounds(0,465,712,40);
-		containerWindow.add(lbl_loading_application_message);
+		lbl_mssg_loading_software = new JLabel();
+		lbl_mssg_loading_software.setLocation(0, 465);
+		lbl_mssg_loading_software.setSize(380, 40);
+		lbl_mssg_loading_software.setText("Cargando aplicación");
+		lbl_mssg_loading_software.setFont(new Font("Yu Gothic UI", Font.PLAIN, 20));
+		lbl_mssg_loading_software.setForeground(new Color(255,255,255));
+		lbl_mssg_loading_software.setVerticalAlignment(SwingConstants.TOP);
+		lbl_mssg_loading_software.setHorizontalAlignment(SwingConstants.RIGHT);
+		containerWindow.add(lbl_mssg_loading_software);
+		
+		lbl_animated_loading = new JLabel();
+		lbl_animated_loading.setLocation(390, 465);
+		lbl_animated_loading.setSize(317, 40);
+		lbl_animated_loading.setIcon(icon.ANIMATED_LOADING_SOFTWARE);
+		lbl_animated_loading.setForeground(Color.WHITE);
+		lbl_animated_loading.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 20));
+		lbl_animated_loading.setHorizontalAlignment(SwingConstants.LEFT);
+		lbl_animated_loading.setVerticalAlignment(SwingConstants.CENTER);
+		containerWindow.add(lbl_animated_loading);
 		
 	}
 	
